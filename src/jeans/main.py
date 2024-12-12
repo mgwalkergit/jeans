780257f1dd785d2fa2c9cc27849973ba0e8f9894
Matthew Walker <mgwalker@m005.opa.vera.psc.edu>
2024-12-12 00:17:23 -0500

    bug fix

--- src/jeans/main.py
+++ src/jeans/main.py
@@ -30,9 +30,9 @@ def get_nfw_scale(triangle,h,m_triangle,c_triangle):#r_triangle, scale radius r_
 def get_dehnen_core_scale(triangle,h,m_triangle,c_triangle):#r_triangle, scale radius r_s and scale density rho_s, of NFW halo, units of pc and U(m_triangle) / u(r_scale)**3
     gc=get_dehnen_core_gc(c_triangle)
     if type(m_triangle) is ap.quantity.Quantity:
-        r_triangle=((2.*g*m_triangle/triangle/h0**2)**(1/3)).to(u.pc)#r_triangle in units of pc, where triangle is overdensity factor = [M_triangle /    else:
+        r_triangle=((2.*g*m_triangle/triangle/h0**2)**(1/3)).to(u.pc)#r_triangle in units of pc, where triangle is overdensity factor = [M_triangle / (4*pi*r_triangle**3)] / rho_crit_0, where rho_crit_0 = 3H_0^2/(8*pi*G), H_0 is hubble constant, m_triangle is given in units of Msun
+    else:
         r_triangle=(2.*g.value*m_triangle/triangle*(1.e+6**2)/(h*100.)**2)**(1/3)#r_triangle in units of pc, where triangle is overdensity factor = [M_triangle / (4*pi*r_triangle**3)] / rho_crit_0, where rho_crit_0 = 3H_0^2/(8*pi*G), H_0 is hubble constant, m_triangle is given in units of Msun
- (4*pi*r_triangle**3)] / rho_crit_0, where rho_crit_0 = 3H_0^2/(8*pi*G), H_0 is hubble constant, m_triangle is given in units of Msun
     r_scale=r_triangle/c_triangle#scale radius in same units as r_triangle, where concentration is defined as c_triangle=r_triangle/r_scale
     return r_triangle,r_scale,gc*m_triangle/(4./3.)/np.pi/r_scale**3
 
