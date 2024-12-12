a0a4afc0726670df7ad753d04bf74663e0bea1d2
Matthew Walker <mgwalker@m005.opa.vera.psc.edu>
2024-12-12 01:08:27 -0500

    bug fix

--- src/jeans/main.py
+++ src/jeans/main.py
@@ -417,9 +417,11 @@ def get_tracer(model,**params):
         def number(x):
             return plum_number(x)
         def luminosity_density(x):
-            return number_density(x)*params['luminosity_tot']/params['r_scale']**3/ntotnorm
+            return number_density(x)*params['luminosity_tot']/plum_ntotnorm()/params['r_scale']**3
+        def luminosity(x):
+            return number_density(x)*params['luminosity_tot']
 
-        return tracer(model=model,luminosity_tot=params['luminosity_tot'],r_scale=params['r_scale'],upsilon=params['upsilon'],nu0=nu0,sigma0=sigma0,nscalenorm=plum_nscalenorm(),ntotnorm=plum_ntotnorm(),rhalf_2d=rhalf_2d,rhalf_3d=rhalf_3d,number_density=number_density,number_density_2d=number_density_2d,number=number,luminosity_density=number_density()*params['luminosity_tot']/plum_ntotnorm()/params['r_scale']**3,luminosity=number()*params['luminosity_tot'])
+        return tracer(model=model,luminosity_tot=params['luminosity_tot'],r_scale=params['r_scale'],upsilon=params['upsilon'],nu0=nu0,sigma0=sigma0,nscalenorm=plum_nscalenorm(),ntotnorm=plum_ntotnorm(),rhalf_2d=rhalf_2d,rhalf_3d=rhalf_3d,number_density=number_density,number_density_2d=number_density_2d,number=number,luminosity_density=luminosity_density,luminosity=luminosity)
 
     if model=='exp':
 
