7cd99e545ca52930622f336a698d5c5422485f48
Matthew Walker <mgwalker@m005.opa.vera.psc.edu>
2024-12-15 17:27:11 -0500

    trying to get UFUNC helpers to work

--- src/jeans/main.py
+++ src/jeans/main.py
@@ -6,7 +6,8 @@ import warnings
 import matplotlib.pyplot as plt
 import astropy as ap
 import astropy.units as u
-from astropy.units.quantity_helper import UFUNC_HELPERS, helper_dimensionless_to_dimensionless
+from astropy.units.quantity_helper import UFUNC_HELPERS#,helper_dimensionless_to_dimensionless
+from astropy.units.quantity_helper.helpers import helper_dimensionless_to_dimensionless
 UFUNC_HELPERS[scipy.special.erf,scipy.special.modstruve] = helper_dimensionless_to_dimensionless
 
 g=0.004317#newton's G in units of km/s, pc, Msun
