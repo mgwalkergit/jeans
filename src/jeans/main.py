7104f959127b47530fa60ced321caa7932505a6c
Matthew Walker <mgwalker@m005.opa.vera.psc.edu>
2025-01-28 21:39:31 -0500

    addressed singularity in exp_luminosity_density

--- src/jeans/main.py
+++ src/jeans/main.py
@@ -184,9 +184,15 @@ def plum_luminosity_density_2d(x):#Sigma(X) / Sigma0, X=R/r_scale
 
 def exp_luminosity_density(x):#nu(x) / nu_scale, x=r/r_scale
     if type(x) is ap.units.quantity.Quantity:#have to work around problems with scipy.special.modstruve working with quantities
-        return scipy.special.kn(0,x.value)
+        if x.value<1.e-10:
+            return np.inf
+        else:
+            return scipy.special.kn(0,x.value)
     else:
-        return scipy.special.kn(0,x)
+        if x<1.e-10:
+            return np.inf
+        else:
+            return scipy.special.kn(0,x)
 
 def exp_luminosity_density_2d(x):#Sigma(X) / Sigma0, X=R/r_scale
     return np.exp(-x)
